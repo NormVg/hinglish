6499a0392b4e07ac55196d1342c19cd6e9fd4f22
NormVg <87635928+NormVg@users.noreply.github.com>
2024-03-23 00:58:43 +0530

    Update down.py

--- down.py
+++ down.py
@@ -1,12 +1,18 @@
 from pytube import YouTube
+from pydub import AudioSegment
+
 
 yt = YouTube('http://youtube.com/watch?v=2nsAB11WP3g')
 a = yt.streams.get_lowest_resolution().download()
 
-import moviepy.editor as mp
- 
-# Insert Local Video File Path 
-clip = mp.VideoFileClip(a)
- 
-# Insert Local Audio File Path
-clip.audio.write_audiofile(r"main.wav")
+
+
+def convert_mp4_to_wav(mp4_file, wav_file):
+    # Load the MP4 file
+    audio = AudioSegment.from_file(mp4_file, format="mp4")
+    
+    # Convert the audio to WAV format
+    audio.export(wav_file, format="wav")
+
+# Replace 'input.mp4' and 'output.wav' with your input and output file paths
+convert_mp4_to_wav(a, 'main.wav')
